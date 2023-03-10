8e110626b32f727ffbe84bf4e10c9e14ca0c3cc7
spencerdearman <121642493+spencerdearman@users.noreply.github.com>
2023-03-10 14:56:21 -0600

    First commit

--- Divemeets-Parser/Divemeets-Parser/HTMLParser.swift
+++ Divemeets-Parser/Divemeets-Parser/HTMLParser.swift
@@ -9,18 +9,30 @@ import SwiftSoup
 import Foundation
 
 final class HTMLParser{
-    func parse(html: String) {
+    func parse(html: String) -> String {
         do {
             let document: Document = try SwiftSoup.parse(html)
             guard let body = document.body() else {
-                return
+                return ""
             }
             let main = try body.getElementsByTag("body").compactMap({try? $0.html()})
             let html = main[0]
-            print(html)
+            let doc:Document = try SwiftSoup.parse(html)
+            //print(doc)
+            //print(try doc.getElementsByTag("tbody"))
+            //let bodySection = doc.body()
+            //print(bodySection!)
+            //let tds = try doc.getElementsByTag("tr")
+            //print(try tds.text())
+            
+            let myRows : Elements? = try doc.getElementsByTag("tr")
+            try myRows!.forEach({ row in
+                print(try row.text())
+            })
         }
         catch {
             print("Error Parsing: " + String(describing: error))
         }
+        return html
     }
 }
